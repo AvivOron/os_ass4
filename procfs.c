722be3aaea09b4847614c282da8fe4fee9aa85f4
aviv oron <avivoron@gmail.com>
2017-06-14 18:20:00 +0300

    hello

--- procfs.c
+++ procfs.c
@@ -13,12 +13,53 @@
 
 int 
 procfsisdir(struct inode *ip) {
-  return 0;
+	if(ip->type == T_DIR){
+		return 1;
+	}
+	return 0;
+}
+
+void
+procfsiunlock(struct inode *ip)
+{
+  if(ip == 0 || !(ip->flags & I_BUSY) || ip->ref < 1)
+    panic("procfsiunlock");
+
+  acquire(&icache.lock);
+  ip->flags &= ~I_BUSY;
+  wakeup(ip);
+  release(&icache.lock);
 }
 
 void 
 procfsiread(struct inode* dp, struct inode *ip) {
+  struct buf *bp;
+
+
+  if(ip == 0 || ip->ref < 1)
+    panic("procfsiread");
+
+  acquire(&icache.lock);
+  while(ip->flags & I_BUSY)
+    sleep(ip, &icache.lock);
+  ip->flags |= I_BUSY;
+  release(&icache.lock);
+
+  if(!(ip->flags & I_VALID)){
+    ip->dev = dp->dev;
+    ip->major = dp->major;
+    //ip->minor = dp->minor;
+    //ip->nlink = dp->nlink;
+    //ip->size = dp->size;
+
+    ip->flags |= I_VALID;
+    procfsiunlock(ip);
+    if(ip->type == 0)
+      panic("procfsiread: no type");
+  }
 }
+
+
 
 int
 procfsread(struct inode *ip, char *dst, int off, int n) {
